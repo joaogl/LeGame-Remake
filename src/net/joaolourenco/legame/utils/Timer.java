aaf250721a5fd44b7861f4b89f31a482f815f0c2
Joao Lourenco <joaoguerralourenco@hotmail.com>
2015-03-04 20:21:29 +0000

    Added Timers.

---
+++ src/net/joaolourenco/legame/utils/Timer.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2014 Joao Lourenco
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.joaolourenco.legame.utils;
+
+/**
+ * @author Joao Lourenco
+ * 
+ */
+public class Timer implements Runnable {
+
+	/**
+	 * Thread where the timer will run.
+	 */
+	private Thread thread;
+	/**
+	 * Variable that keeps track if the timer is running or not.
+	 */
+	private boolean running = false;
+
+	private String name;
+	private boolean finit = false;
+	private long lastTimer;
+	private int spacing, left;
+	private TimerResult result;
+
+	/**
+	 * 
+	 * @author Joao Lourenco
+	 */
+	public Timer(String name, int spacing, int amout, TimerResult res) {
+		this.name = name;
+		this.spacing = spacing;
+		this.left = amout;
+		this.finit = true;
+		this.result = res;
+		start();
+	}
+
+	public Timer(String name, int spacing, TimerResult res) {
+		this.name = name;
+		this.spacing = spacing;
+		this.finit = false;
+		this.result = res;
+		start();
+	}
+
+	/**
+	 * Method that starts the timer.
+	 * 
+	 * @author Joao Lourenco
+	 */
+	public synchronized void start() {
+		this.thread = new Thread(this, "Timer-" + this.name);
+		this.running = true;
+		this.thread.start();
+	}
+
+	public void run() {
+		this.lastTimer = System.currentTimeMillis();
+		while (this.running) {
+			if (System.currentTimeMillis() - this.lastTimer > this.spacing) {
+				this.lastTimer += this.spacing;
+				call();
+			}
+		}
+	}
+
+	public void call() {
+		if (this.finit) {
+			this.left--;
+			this.result.timerCall();
+			if (this.left <= 0) this.running = false;
+		}
+	}
+
+}
